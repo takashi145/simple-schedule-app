4c75cba746195a07d4efadd1793a56809440579f
takashi145 <t.shuhei0203@gmail.com>
2023-02-24 12:48:20 +0900

    flash message animation

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,8 +1,30 @@
 <script setup>
+import { usePage } from '@inertiajs/vue3';
+import { computed, onMounted, ref, watch } from 'vue';
+
+onMounted(() => {
+  show.value = true;
+  setTimeout(() => {
+    show.value = false;
+  }, 5000);
+})
+
+const flash = computed(() => usePage().props.flash);
+const show = ref(false);
+
+watch(flash, () => {
+  if(flash.value.message) {
+    show.value = true;
+    setTimeout(() => {
+      show.value = false;
+    }, 5000);
+  }
+});
+
 </script>
 <template>
   <transition
-      enter-active-class="ease-out duration-300"
+      enter-active-class="ease-out duration-500"
       enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       enter-to-class="opacity-100 translate-y-0 sm:scale-100"
       leave-active-class="ease-in duration-200"
@@ -10,12 +32,12 @@
       leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
   >
     <div 
-      v-if="$page.props.flash.message"
-      @click="$page.props.flash.message = null"
-      :class="[$page.props.flash.status === 'success' ? 'bg-green-400' : 'bg-red-400']"
+      v-if="show && flash.message"
+      @click="flash.message = null"
+      :class="[flash.status === 'success' ? 'bg-green-400' : 'bg-red-400']"
       class="text-white w-full lg:w-2/3 mx-auto text-center rounded py-2 lg:mt-3 cursor-pointer"
     >
-      {{ $page.props.flash.message }}
+      {{ flash.message }}
     </div>
   </transition>
 </template>
